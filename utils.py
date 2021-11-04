2180fa792d748e2482f9e0dc84b9edc2b4bae674
maksym.valyn <maksym.valyn@starnavi.io>
2021-11-04 22:49:00 +0200

    bug for long investments tables fixed

--- utils.py
+++ utils.py
@@ -71,14 +71,25 @@ class ITDashBoardWebDataScraper:
         'Nuclear_Regulatory_Commission': '/div/div[9]/div[2]/div/div/div/a'
     }
     URL = "https://itdashboard.gov"
+
     DIVE_IN_BUTTON = '//*[@id="node-23"]/div/div/div/div/div/div/div/a'
+
     AGENCIES_TILES = '//*[@id="agency-tiles-widget"]'
     AGENCY_LOCATOR = ALL_AGENCIES[f'{SELECTED_AGENCY}']
     AGENCY_BUTTON = f'{AGENCIES_TILES}{AGENCY_LOCATOR}'
+
     INVESTMENTS_TABLE = '//*[@id="investments-table-object"]'
     INVESTMENTS_TABLE_INFO = '//*[@id="investments-table-object_info"]'
-    TABLE_SHOW_ALL_BUTTON = '//*[@id="investments-table-object_length"]/label/select/option[4]'
+
     PDF_FILE_LINK = '//*[@id="business-case-pdf"]/a'
+
+    TABLE_SHOW_50_ENTRIES_BUTTON = '//*[@id="investments-table-object_length"]/label/select/option[3]'
+
+    TABLE_NEXT_BUTTON = '//*[@id="investments-table-object_next"]'
+
+    TABLE_FIRST_BUTTON = '//*[@id="investments-table-object_first"]'
+
+    INVESTMENTS_TABLE_ROWS_SHOWN = 50
 
     file_processor = FileProcessor
 
@@ -86,43 +97,76 @@ class ITDashBoardWebDataScraper:
         browser_lib.wait_until_element_is_visible(locator, timeout=30)
         browser_lib.click_element_when_visible(locator)
 
-    def _get_investment_table_total_entries(self):
-        browser_lib.wait_until_page_contains_element(self.INVESTMENTS_TABLE, timeout=30)
-        table_info = browser_lib.get_text(self.INVESTMENTS_TABLE_INFO)
-        return int(table_info.split()[-2])
+    def _wait_until_element_is_ready(self, locator):
+        browser_lib.wait_until_element_is_visible(locator, timeout=30)
+        browser_lib.wait_until_page_contains_element(locator, timeout=30)
 
-    def _click_show_all_entries_button(self):
-        self._click_element(self.TABLE_SHOW_ALL_BUTTON)
-        total_entries = self._get_investment_table_total_entries()
-        table_info = f'Showing 1 to {total_entries} of {total_entries} entries'
+    def _click_next_table_pagination_button(self):
+        self._click_element(f'{self.TABLE_NEXT_BUTTON}')
+
+    def _click_first_table_pagination_button(self):
+        self._click_element(f'{self.TABLE_FIRST_BUTTON}')
+
+    def _get_investments_table_info(self):
+        browser_lib.wait_until_page_contains_element(self.INVESTMENTS_TABLE, timeout=30)
+        return browser_lib.get_text(self.INVESTMENTS_TABLE_INFO)
+
+    def _is_last_page_of_investments_table(self):
+        table_info = self._get_investments_table_info()
+        entries_start = table_info.split()[3]
+        entries_end = table_info.split()[5]
+        return entries_start == entries_end
+
+    def _get_investments_table_total_entries(self):
+        table_info = self._get_investments_table_info()
+        text = table_info.split()[-2]
+        value = int(text.replace(',', ''))
+        return (text, value)
+
+    def _get_investments_table_entries(self):
+        table_info = self._get_investments_table_info()
+        start = int(table_info.split()[1].replace(',', ''))
+        stop = int(table_info.split()[3].replace(',', ''))
+        rows = stop - start + 1
+        return rows
+
+    def _click_show_50_entries_button(self):
+        self._click_element(self.TABLE_SHOW_50_ENTRIES_BUTTON)
+        total_entries = self._get_investments_table_total_entries()
+        table_info = f'Showing 1 to {self.INVESTMENTS_TABLE_ROWS_SHOWN} of {total_entries[0]} entries'
+        if total_entries[1] < self.INVESTMENTS_TABLE_ROWS_SHOWN:
+            table_info = f'Showing 1 to {total_entries[0]} of {total_entries[0]} entries'
         browser_lib.wait_until_element_contains(self.INVESTMENTS_TABLE_INFO, text=table_info, timeout=30)
 
     def _get_investments_table_dimensions(self):
         table_locator = self.INVESTMENTS_TABLE
-        browser_lib.wait_until_element_is_visible(table_locator, timeout=30)
-        browser_lib.wait_until_page_contains_element(table_locator, timeout=30)
+        self._wait_until_element_is_ready(table_locator)
         rows_number = browser_lib.get_element_count(f'{table_locator}/tbody/tr')
         columns_number = browser_lib.get_element_count(f'{table_locator}/tbody/tr/td') // rows_number
         return (rows_number, columns_number)
 
     def _get_tile_elements_count(self):
         agencies_locator = self.AGENCIES_TILES
-        browser_lib.wait_until_element_is_visible(agencies_locator, timeout=30)
-        browser_lib.wait_until_page_contains_element(agencies_locator, timeout=30)
+        self._wait_until_element_is_ready(agencies_locator)
         blocks = browser_lib.get_element_count(f'{agencies_locator}/div/div')
         tiles_in_block = browser_lib.get_element_count(f'{agencies_locator}/div/div[1]/div')
         return (blocks, tiles_in_block)
 
     def _pdf_file_link_generator(self):
         browser_lib.switch_browser('main')
-        total_entries = self._get_investment_table_total_entries()
-        for row in range(1, total_entries + 1):
-            browser_lib.switch_browser('main')
-            file_locator = f'{self.INVESTMENTS_TABLE}/tbody/tr[{row}]/td[1]/a'
-            try:
-                yield browser_lib.get_element_attribute(file_locator, attribute='href')
-            except ElementNotFound:
-                continue
+        self._click_first_table_pagination_button()
+        while True:
+            table_entries = self._get_investments_table_entries()
+            for row in range(1, table_entries + 1):
+                browser_lib.switch_browser('main')
+                file_locator = f'{self.INVESTMENTS_TABLE}/tbody/tr[{row}]/td[1]/a'
+                try:
+                    yield browser_lib.get_element_attribute(file_locator, attribute='href')
+                except ElementNotFound:
+                    continue
+            if self._is_last_page_of_investments_table():
+                break
+            self._click_next_table_pagination_button()
 
     def _download_pdf_file(self, link):
         file_name = f'{link.split("/")[-1]}.pdf'
@@ -160,17 +204,23 @@ class ITDashBoardWebDataScraper:
         browser_lib.switch_browser('main')
 
     def investments_table_data_generator(self):
-        self._click_show_all_entries_button()
+        self._click_show_50_entries_button()
         data = {}
         empty_rows_amount = 2
-        rows_number, columns_number = self._get_investments_table_dimensions()
-        for row in range(1, rows_number + 1):
-            for column in range(1, columns_number + 1):
-                value = browser_lib.get_table_cell(self.INVESTMENTS_TABLE, row + empty_rows_amount, column)
-                data['row'] = row
-                data['column'] = column
-                data['value'] = value
-                yield data
+        excel_rows_offset = 0
+        while True:
+            rows_number, columns_number = self._get_investments_table_dimensions()
+            for row in range(1, rows_number + 1):
+                for column in range(1, columns_number + 1):
+                    value = browser_lib.get_table_cell(self.INVESTMENTS_TABLE, row + empty_rows_amount, column)
+                    data['row'] = row + excel_rows_offset
+                    data['column'] = column
+                    data['value'] = value
+                    yield data
+            if self._is_last_page_of_investments_table():
+                break
+            excel_rows_offset += self.INVESTMENTS_TABLE_ROWS_SHOWN
+            self._click_next_table_pagination_button()
 
     def close_all_browsers(self):
         browser_lib.close_all_browsers()
